83bf3b99e8694a4a686b818e907645a726c8dd85
Kostiantyn Lapin <lapin.kostiantyn@gmail.com>
2026-03-07 19:03:42 +0100

    feat(backend): add Swagger for API documentation.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,14 +2,17 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {  RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
-
+@ApiTags('Auth') 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService) { }
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' }) 
+  @ApiBody({ type: RegisterDto })  
   async register(@Body() registerDto: RegisterDto) {
    
       return await this.authService.register(registerDto);  
@@ -17,6 +20,8 @@ export class AuthController {
   }
 
   @Post('login')
+  @ApiOperation({ summary: 'Login with email and password' })
+  @ApiBody({ type: LoginDto })
   async login(@Body() loginDto: LoginDto) {
     
       return await this.authService.login(loginDto);
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+ if (process.env.NODE_ENV !== 'production') { 
+  const config = new DocumentBuilder()
+    .setTitle('API Example')       
+    .setDescription('API description') 
+    .setVersion('0.0.1')        
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+ }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
