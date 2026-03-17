32df60e9363badbc9e94321c4ffbf4c9530d9a95
Kostiantyn Lapin <lapin.kostiantyn@gmail.com>
2026-03-17 17:46:20 +0100

    fix(frontend): fix layout EventsPage when  No events match the selected tags.

--- frontend/src/pages/EventsPage.tsx
+++ frontend/src/pages/EventsPage.tsx
@@ -18,7 +18,7 @@ export const EventsPage = () => {
   const { events, loading } = useSelector((state: RootState) => state.events);
 
   const [tempTags, setTempTags] = useState<Tag[]>([]);
-  
+
 
   useEffect(() => {
     dispatch(getEvents());
@@ -34,55 +34,60 @@ export const EventsPage = () => {
     dispatch(getEvents());
   };
   return (
-    <div className="max-w-6xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6">Public Events</h1>
+   <div className="max-w-6xl mx-auto p-6 flex-1 flex flex-col">
+       
+        <h1 className="text-3xl font-bold mb-6">Public Events</h1>
 
+        
+        <div className="max-w-3xl mb-6">
+          <div className="flex flex-col sm:flex-row gap-2 sm:items-start">
+            {/* SELECT */}
+            <div className="w-full sm:flex-1">
+              <TagMultiSelect
+                availableTags={availableTags}
+                selectedTags={tempTags}
+                onChange={setTempTags}
+                placeholder="Filter events by tags"
+              />
+            </div>
 
-      <div className="max-w-3xl mb-6">
-        <div className="flex flex-col sm:flex-row gap-2 sm:items-start">
+            {/* BUTTONS */}
+            <div className="flex gap-2">
+              <button
+                onClick={applyFilter}
+                className="flex-1 sm:flex-none px-5 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+              >
+                Apply
+              </button>
 
-          {/* SELECT */}
-          <div className="w-full sm:flex-1">
-            <TagMultiSelect
-              availableTags={availableTags}
-              selectedTags={tempTags}
-              onChange={setTempTags}
-              placeholder="Filter events by tags"
-            />
+              <button
+                onClick={clearFilter}
+                className="flex-1 sm:flex-none px-5 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition"
+              >
+                Clear
+              </button>
+            </div>
           </div>
+        </div>
 
-          {/* BUTTONS */}
-          <div className="flex gap-2">
-            <button
-              onClick={applyFilter}
-              className="flex-1 sm:flex-none px-5 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-            >
-              Apply
-            </button>
-
-            <button
-              onClick={clearFilter}
-              className="flex-1 sm:flex-none px-5 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 transition"
-            >
-              Clear
-            </button>
-          </div>
+       
+        <div className="flex-1 flex flex-col">
+          {loading ? (
+            <div className="flex-1 flex items-center justify-center text-center w-full">
+              Loading events...
+            </div>
+          ) : events.length === 0 ? (
+            <div className="flex-1 flex items-center justify-center text-center w-full text-gray-500">
+              No events match the selected tags.
+            </div>
+          ) : (
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 flex-1">
+              {events.map((event) => (
+                <EventCard key={event.id} event={event} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
-
-      {loading ? (
-        <div className="text-center p-10">Loading events...</div>
-      ) : events.length === 0 ? (
-        <div className="text-center text-gray-500 mt-10">
-          No events match the selected tags.
-        </div>
-      ) : (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {events.map(event => (
-            <EventCard key={event.id} event={event} />
-          ))}
-        </div>
-      )}
-    </div>
   );
 };
