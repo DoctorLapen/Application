1fd1e2774fa840a76a1c926bfcb21aabb6a0fc42
Kostiantyn Lapin <lapin.kostiantyn@gmail.com>
2026-04-09 20:49:51 +0200

    feat(backend): add cors origins list

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,30 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+
+ const allowedOrigins = process.env.CORS_ORIGINS
+  ? process.env.CORS_ORIGINS.split(',').map(origin => origin.trim())
+  : [];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "*"
-  }); 
+  
+  
+
+app.enableCors({
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error('Not allowed by CORS'));
+  },
+});
    app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,               
